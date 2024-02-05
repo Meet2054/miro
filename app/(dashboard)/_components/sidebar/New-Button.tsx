0f3692c7ebb0f701751be24ed241d32cc3b60912
Meet2054 <meetaparekh2003@gmail.com>
2024-02-06 01:58:13 +0530

    Side bar updates

--- app/(dashboard)/_components/sidebar/New-Button.tsx
+++ app/(dashboard)/_components/sidebar/New-Button.tsx
@@ -11,9 +11,20 @@ import { Dialog,
 
 const NewButton = () => {
   return (
-    <div>
+    <Dialog>
+        <DialogTrigger asChild>
+            <div className="aspect-squre">
+            <button className='bg-white/25 h-full w-full rounded-md 
+            flex justify-center items-center opacity-60 hover:opacity-100 transition '>
+                <Plus className="text-white "/>
+            </button>
+            </div>
+        </DialogTrigger>
+        <DialogContent className="p-0 bg-transparent boarder-none max-w-[480px] ">
+            <CreateOrganization />  
+        </DialogContent>
       
-    </div>
+    </Dialog>
   )
 }
 
